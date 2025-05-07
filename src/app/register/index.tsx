5e3d79044fba17bd4f02d2e7247e9eb224f14c21
IamThiago-IT <thiagodossantos315@gmail.com>
2025-05-07 10:33:21 +0000

    feat: add login and registration screens for ecomCars+ app
    - Implemented the main login screen with email and password inputs, and a link to the registration page.
    - Created a registration screen with inputs for name, email, and password.
    - Styled both screens to maintain a consistent look and feel with the app branding.

---
+++ src/app/register/index.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        <Text style={{ color: '#000' }}>ecom</Text>
+        <Text style={{ fontWeight: 'bold', color: '#A07A28' }}>Cars+</Text>
+      </Text>
+      <Text style={styles.subtitle}>Registar</Text>
+      <TextInput placeholder='Nome' style={styles.input} />
+      <TextInput placeholder='Email' style={styles.input} />
+      <TextInput placeholder='Senha' style={styles.input} secureTextEntry={true} />
+      <Text style={styles.button}>Entrar no ecomCars+</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 24,
+    marginBottom: 24,
+    fontWeight: 'medium',
+    color: '#A07A28',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: '#A07A28',
+    borderWidth: 3,
+    borderRadius: 100,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: '#A07A28',
+    color: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 100,
+    textAlign: 'center',
+    marginTop: 10,
+    width: '80%',
+    textAlignVertical: 'center',
+  },
+});
